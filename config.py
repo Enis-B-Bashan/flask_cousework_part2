95a9f84f55fcd8b86757feb372709e8871a9fbef
EnisBerkayBASHAN <enis.berkay.bashan@gmail.com>
2025-10-22 17:37:32 +0100

    init

---
+++ config.py
@@ -0,0 +1,9 @@
+
+
+class Config:
+    SECRET_KEY = 'devkey123'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+    TESTING = True
+
